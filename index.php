0c2885de35c9db66efa3a559f09be9a45b59d164
Dishkan <DH.17@mail.ru>
2020-11-21 15:47:03 +0500

    Simple Landing page

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Helix Original</title>
+
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link href="css/style.css" rel="stylesheet">
+
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+	
+	<section id="header">
+		<div class="section-table">
+			<div class="section-row">
+				<div class="section-cell">
+					<div class="section-center">
+						<p class="logo">
+							<img src="img/helix2.png" alt="logo">
+						</p>
+						<h1 >Helix Original</h1>
+						<p>Это клинически проверенная формула, которая успокаивает воспаленные, ноющие суставы, предотвращает и помогает поддерживать гибкость!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<section id="speakers">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 wow fadeInDown">
+					<h2>Главные задачи лекарства</h2>
+					<p class="large">Все компоненты играют в формуле Helix играют важную роль в смазке соединений для простоты движения. Он также питает соединительную ткань, чтобы помочь восстановить и восстановить изношенные суставы. Являетесь ли вы лучшим спортсменом или энтузиастом фитнеса, который принимает добавки для силы, выносливости и оставаться в форме и здоровым. </p>
+				</div>
+				<div class="col-md-6 row-item-col wow fadeInDown">
+					<div class="section-tabs">
+						<!-- Nav tabs -->
+						<ul class="nav nav-tabs nav-justified" role="tablist">
+							<li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Гарантия на 100%</a></li>
+							<li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Для кого препарат?</a></li>
+						</ul>
+
+						<!-- Tab panes -->
+						<div class="tab-content clearfix">
+							<div role="tabpanel" class="tab-pane fade in active" id="home">
+								<p>Наши клиенты остались довольны после заказов и рекомендуют также другим. В случае если вы не поправитесь после применения лекарства, ваши средтсва будут возмещены!</p>
+								<ul class="list-unstyled pull-left">
+									<li><i class="fa fa-check-circle"></i>Печать из Министерство здровохранения республики Узбекистан</li>
+									<li><i class="fa fa-check-circle"></i>Сертификат Соответсвия</li>
+									<li><i class="fa fa-check-circle"></i>Хорошие отзывы клиентов</li>
+								</ul>
+							</div>
+							<div role="tabpanel" class="tab-pane" id="profile">
+								<ul class="list-unstyled pull-left">
+									<li><i class="fa fa-check-circle"></i>Боли в суставах, ногах, руках</li>
+									<li><i class="fa fa-check-circle"></i>Артрит</li>
+									<li><i class="fa fa-check-circle"></i>Полеартрит</li>
+									<li><i class="fa fa-check-circle"></i>Ревматизм</li>
+									<li><i class="fa fa-check-circle"></i>Артроз</li>
+									<li><i class="fa fa-check-circle"></i>Сахарный диабет</li>									
+								</ul>
+							</div>
+						</div>
+					</div><!-- /.section-tabs -->
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="events">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 row-item-col wow fadeInDown">
+					<h2>Способо применения</h2>
+					<p class="large">Рано утром, по одной капсуле, до еды за день выпить со стаканом воды и повторять процесс регулярно по утрам в течении 3 месяцев. После этого срока вы почуствуете 100% силу и восстоновление вашего здоровье.</p>
+				</div>
+				<div class="col-md-6 wow fadeInDown">
+					<div class="section-tabs">
+						<!-- Nav tabs -->
+						<ul class="nav nav-tabs nav-justified" role="tablist">
+							<li role="presentation" class="active"><a href="#nav18" aria-controls="home" role="tab" data-toggle="tab">Расписание</a></li>
+						</ul>
+
+						<!-- Tab panes -->
+						<div class="tab-content clearfix">
+							<div role="tabpanel" class="tab-pane fade in active" id="nav18">
+								<p class="tab-collapse clearfix">
+									<span class="time">08:00 AM</span>
+									<a role="button" data-toggle="collapse" href="#" aria-expanded="false" aria-controls="collapseExample" class="collapse-link">С утра, до еды, выпить одну капсулу за день!</a>
+							</div>
+
+						</div>
+					</div><!-- /.section-tabs -->
+				</div>
+			</div>
+			<div class="row row-item">
+				<div class="col-md-6 row-item-col wow fadeInDown">
+					<div class="embed-responsive embed-responsive-16by9">
+						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Sh85jKmNhaE"></iframe>
+					</div>
+				</div>
+				<div class="col-md-6 wow fadeInDown">
+					<h2>Рекламное видео</h2>
+					<p class="large">Здесь вы можете посмотреть визуально, на что влияет лекарство и как помогает, улучшает состояние больных спустя определенное время.</p>
+				</div>
+			</div>
+			<div class="row row-item">
+				<div class="col-md-6 row-item-col wow fadeInDown">
+					<h2>О лекарстве</h2>
+					<p class="large">СДЕЛАНО С БЕЛКОМ КРИТОВОГО УЛИЧКА: Наша оригинальная добавка для поддержки суставов Helix изготовлена ​​из натурального белка улитки, расположенной на острове Крит. Содержа аллантоин и другие питательные вещества при остеоартрите, улитка на Крите - единственное животное в мире, способное вырастить свою раковину всего за 7 дней. С Helix Original эта улитка может вырастить ваши суставы как новые.
+ЗНАЧЕНИЕ ПРИРОДНЫХ ИНГРЕДИЕНТОВ: Наряду с белком из улитки Крита, наша добавка Helix Original Join Support дополняется противовоспалительной куркумой, регенерирующим глюкозамином, смазывающим хондроитином и эффективным Boswellia Serrata. В Helix Original нашей целью является предоставление натуральных решений без лекарств без побочных эффектов или долгосрочных рисков.
+ВОССТАНОВИТЕ СОЕДИНЕНИЕ, КОСТЬ И ЗДОРОВЬЕ ХРАНИЛИЩА: Живите как пожилые люди на Крите с нашим дополнением Helix Original Join Support Наслаждайтесь жизнью без боли в суставах; делай что хочешь, когда хочешь. Восстановите хрящи, сухожилия, связки и другие соединительные ткани, чтобы вы могли восстановить захватывающую и подвижную жизнь, которой вы привыкли жить.
+ПО СБРОСУ СЧЕТЧИКА: Мы снимаем стресс с аптек и врачебных кабинетов, предоставляя без рецепта оригинальную добавку поддержки суставов Helix. Проверенные клиническими испытаниями, наши безрецептурные добавки для суставов дадут вам жизнь без боли в суставах и дискомфорта за небольшую плату. Прекратите тратить целое состояние на рецепты и переключитесь на естественное облегчение.</p>
+				</div>
+				<div class="col-md-6 row-item-col wow fadeInDown">
+					<div id="carousel" class="carousel slide" data-ride="carousel">
+						<!-- Indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="#carousel" data-slide-to="0" class="active"></li>
+							<li data-target="#carousel" data-slide-to="1"></li>
+						</ol>
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner" role="listbox">
+							<div class="item active">
+								<img src="img/certificate.jpg" alt="">
+							</div>
+							<div class="item">
+								<img src="img/certificate2.jpg" alt="">
+							</div>
+						</div><!-- carousel-inner -->
+					</div><!-- #carousel -->
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="contacts">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 row-item-col">
+					<h3><span>5000</span> Людей уже применяют этот препарат по всему Узбекистану</h3>
+				</div>
+				<div class="col-md-3 col-sm-6 row-item-col">
+					<h4>Хотите заказать?</h4>
+					<p>Если хотите заказать наш препарат, вы можете смело позвонить нашим операторам, которые работают с 09:00 до 21:00 каждый день</p>
+				</div>
+				<div class="col-md-3 col-sm-6 row-item-col">
+					<h4>Свяжитесь с нами</h4>
+					<div class="contacts">
+						<p><i class="fa fa-phone-square"></i> +998-91-421-55-50</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="copy">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+			<p>Copyright &copy; <?=date("Y")?></p>
+                 </div>
+            <div class="col-md-6">
+			<p>Created by Dishkan</p>
+                 </div>
+		 </div>
+		</div>
+	</section>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/validator.min.js"></script>
+	<script src="js/select2.full.min.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/scripts.js"></script>
+</body>
+</html>
